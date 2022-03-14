e6937443a6c07e6c45176f430aece0483dec2f33
giorgios17 <giosur@live.it>
2022-03-14 14:01:07 +0100

    ex 2 done

--- js/script.js
+++ js/script.js
@@ -20,12 +20,14 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
+//function get random number on a range
 
 function numeroRandomRange(min, max){
     const range = (max - min) + 1;
     const numeroRandom = Math.floor(Math.random()*range + min);
     return numeroRandom; 
 }
+
 
 const numeroBot = numeroRandomRange(1, 5);
 console.log('Il numero del computer è', numeroBot);
@@ -37,9 +39,13 @@ const numeroUtente = parseInt(prompt('Scegli un numero da 1 a 5'));
 console.log('Hai scelto', oddOrEvenUser);
 console.log('Hai scelto il numero', numeroUtente);
 
-const somma = numeroUtente + numeroBot;
+const somma =  numeroUtente + numeroBot;
 
-console.log(somma)
+const sommaOddOrEven = oddOrEven(somma)
+
+console.log('La somma dei due numeri è', somma)
+
+//function is odd or even
 
 function oddOrEven(num){
     if(num % 2 === 0){
@@ -48,4 +54,11 @@ function oddOrEven(num){
     else{
         return 'odd';
     }
+}
+
+if((sommaOddOrEven === 'odd' && oddOrEvenUser === 'dispari') || (sommaOddOrEven === 'even' && oddOrEvenUser === 'pari')){
+    console.log('Hai vinto')
+}
+else{
+    console.log('Il computer vince')
 }
